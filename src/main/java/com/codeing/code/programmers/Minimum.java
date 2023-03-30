e42ec7518e64165dc47aaca660eb146738aac389
이순우 <renzo.1980@jobkorea.co.kr>
2023-03-30 10:31:21 +0900

    [coding] 최솟값 구하기 https://angryfullstack.tistory.com/

--- src/main/java/com/codeing/code/programmers/Minimum.java
+++ src/main/java/com/codeing/code/programmers/Minimum.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * 배열 A, B의 크기 : 1,000 이하의 자연수
  * 배열 A, B의 원소의 크기 : 1,000 이하의 자연수
- * https://school.programmers.co.kr/learn/courses/30/lessons/12941
+ * 기
  * 최솟값
  */
 public class Minimum {
@@ -14,9 +14,9 @@ public class Minimum {
         int answer = 0;
         Arrays.sort(A);
         Arrays.sort(B);
-        int num = A.length -1;
-        for (int i = 0; i <A.length ; i++) {
-            answer += (A[i] * B[num--]);
+        int num = A.length -1;  //index 역으로 접근하기위한 -1
+        for (int i = 0; i <A.length; i++) {
+            answer += (A[i] * B[num--]);    //총인덱스 접근부터 --
         }
 
         return answer;
